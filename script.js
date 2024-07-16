b4b270bbc14e84748d038a141b0b5bfaeae63d2f
Leonardo Martins <117536177+LeoMartt@users.noreply.github.com>
2024-07-15 22:53:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-var heart = document.querySelector('.heart'),
-    reload = document.querySelector('.reload'),
-    hearts = document.querySelector('.hearts'),
-    allHearts = document.querySelectorAll('.hearts div'),
-    heartAnime = document.querySelector('.heart.anime'),
-    images = document.querySelectorAll('img.hidden'); // Seleciona as imagens ocultas
+document.addEventListener("DOMContentLoaded", function() {
+  var heart = document.querySelector('.heart'),
+      reload = document.querySelector('.reload'),
+      hearts = document.querySelector('.hearts'),
+      allHearts = document.querySelectorAll('.hearts div'),
+      heartAnime = document.querySelector('.heart.anime');
 
-heart.addEventListener('click', animation);
-reload.addEventListener('click', refresh);
+  heart.addEventListener('click', animation);
+  reload.addEventListener('click', refresh);
 
-function animation() {
-   heart.classList.add("active");
-   reload.setAttribute("style", "opacity: 1; cursor: pointer; pointer-events: auto;");
-   heartAnime.style.opacity = "0";
+  function animation() {
+    heart.classList.add("active");
+    reload.style.opacity = "1";
+    reload.style.cursor = "pointer";
+    reload.style.pointerEvents = "auto";
+    heartAnime.style.opacity = "0";
 
-   allHearts.forEach(function(element) {
+    allHearts.forEach(function(element) {
       element.classList.add("active");
-   });
+    });
 
-   images.forEach(function(img) {
-      img.classList.remove('hidden'); // Remove a classe 'hidden'
-      img.classList.add("visible"); // Adiciona a classe 'visible'
-   });
-}
+    var images = document.querySelectorAll('img.hidden'); // Re-seleciona as imagens
+    images.forEach(function(img) {
+      img.classList.remove('hidden');
+      img.classList.add("visible");
+    });
+  }
 
-function refresh() {
-   heart.classList.remove("active");
-   reload.setAttribute("style", "opacity: 0; pointer-events: none;");
-   heartAnime.style.opacity = "1";
+  function refresh() {
+    heart.classList.remove("active");
+    reload.style.opacity = "0";
+    reload.style.pointerEvents = "none";
+    heartAnime.style.opacity = "1";
 
-   allHearts.forEach(function(element) {
+    allHearts.forEach(function(element) {
       element.classList.remove("active");
-   });
+    });
 
-   images.forEach(function(img) {
-      img.classList.remove("visible"); // Remove a classe 'visible'
-      img.classList.add('hidden'); // Adiciona a classe 'hidden'
-   });
-}
+    var images = document.querySelectorAll('img.visible'); // Re-seleciona as imagens visíveis
+    images.forEach(function(img) {
+      img.classList.remove("visible");
+      img.classList.add('hidden');
+    });
+  }
+});
